4d01a64e9030e36a2aaa7e7c0cf9188e4b9f1af7
onursonmez <tavhane@gmail.com>
2022-12-06 00:52:42 +0300

    preload lohout tap

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -136,7 +136,7 @@
 							<div class:hidden={hiddenProfileMenu} class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
 								<!-- Active: "bg-gray-100", Not Active: "" -->
 								<a href="/member/dashboard" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Hesabım</a>
-								<a data-sveltekit-preload-data="off" href="/auth/logout" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Güvenli Çıkış</a>
+								<a data-sveltekit-preload-data="tap" href="/auth/logout" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Güvenli Çıkış</a>
 							</div>
 						{/if}
 					</div>
