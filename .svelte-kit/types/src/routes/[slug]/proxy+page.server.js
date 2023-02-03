4a1f76cd01b3ee8795bfd62ed65e84f6433c4966
onursonmez <tavhane@gmail.com>
2023-02-03 14:48:21 +0300

    detail 404

--- .svelte-kit/types/src/routes/[slug]/proxy+page.server.js
+++ .svelte-kit/types/src/routes/[slug]/proxy+page.server.js
@@ -1,10 +1,14 @@
 // @ts-nocheck
 import * as api from '$lib/api';
+import { error } from '@sveltejs/kit';
 
 /** @param {Parameters<import('./$types').PageLoad>[0]} event */
 export async function load({ locals, params })
 {
     const user = await api.get('user/detail?username=' + params.slug, locals.auth?.token)
+    if(user.code !== 200){
+        throw error(404);
+    }
     const prices = await api.get('price/' + user.result.id, locals.auth?.token)
     const locations = await api.get('location/' + user.result.id, locals.auth?.token)
     const comments = await api.get('comment/' + user.result.id, locals.auth?.token)
--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -1,9 +1,13 @@
 import * as api from '$lib/api';
+import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ locals, params })
 {
     const user = await api.get('user/detail?username=' + params.slug, locals.auth?.token)
+    if(user.code !== 200){
+        throw error(404);
+    }
     const prices = await api.get('price/' + user.result.id, locals.auth?.token)
     const locations = await api.get('location/' + user.result.id, locals.auth?.token)
     const comments = await api.get('comment/' + user.result.id, locals.auth?.token)
