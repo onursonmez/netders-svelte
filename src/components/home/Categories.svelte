74a18f6ddf5337d703c9d2d891197dd5a886d1e4
onursonmez <tavhane@gmail.com>
2023-02-04 11:52:42 +0300

    home categories and how it works

--- src/components/home/Categories.svelte
+++ src/components/home/Categories.svelte
@@ -3,124 +3,71 @@
 </script>
 
 
-<section class="dark:bg-gray-900">
+<section>
 	<div class="block p-6 rounded-lg shadow-md bg-white">
 		<h2 class="mb-4 text-2xl font-bold tracking-tight leading-none lg:text-3xl dark:text-white">Özel ders kategorileri</h2>
 		<p class="text-gray-700 text-base mb-4">
 			Alanında tecrübeli öğretmenlerden özel ders alarak, ihtiyacın olan doğru eğitimi, en uygun fiyatlarla alabilirsin.
 		</p>
 
-		<div>
-			<Splide options={ {
-				arrows: true,
-				gap: '1em',
-				type   : 'loop',
-				interval: 3000,
-				autoplay: true,
-				perPage: 6,
-				updateOnMove : true,
-				breakpoints: {
-					1024: {
-					  perPage: 3,
+		<div class="text-center grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-7 gap-6 p-6">
+			<a href="/ozel-ders-ilanlari-verenler/ilkogretim-takviye" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-ilkogretim.svg" alt="İlköğretim Takviye"/>
+				<span>İlköğretim Takviye</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/lise-takviye" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-lise.svg" alt="Lise Takviye"/>
+				<span>Lise Takviye</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/universite-takviye" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-universite.svg" alt="Üniversite Takviye"/>
+				<span>Üniversite Takviye</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/yabanci-dil" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-yabancidil.svg" alt="Yabancı Dil"/>
+				<span>Yabancı Dil</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/bilgisayar" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-bilgisayar.svg" alt="Bilgisayar"/>
+				<span>Bilgisayar</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/dans" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-dans.svg" alt="Dans"/>
+				<span>Dans</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/direksiyon" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-direksiyon.svg" alt="Direksiyon"/>
+				<span>Direksiyon</span>
+			</a>
 
-					},
-					767: {
-					  perPage: 2,
-
-					},
-					640: {
-					  perPage: 1,
-
-					},
-				  },
-				perMove: 1,
-				pagination: false,
-			  } } aria-label="Kategoriler">
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/yabanci-dil" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-yabancidil.svg" alt="Yabancı Dil"/>
-						<span>Yabancı Dil</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/bilgisayar" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-bilgisayar.svg" alt="Bilgisayar"/>
-						<span>Bilgisayar</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/dans" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-dans.svg" alt="Dans"/>
-						<span>Dans</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/direksiyon" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-direksiyon.svg" alt="Direksiyon"/>
-						<span>Direksiyon</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/ilkogretim-takviye" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-ilkogretim.svg" alt="İlköğretim Takviye"/>
-						<span>İlköğretim Takviye</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/kisisel-gelisim" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-kisiselgelisim.svg" alt="Kişisel Gelişim"/>
-						<span>Kişisel Gelişim</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/lise-takviye" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-lise.svg" alt="Lise Takviye"/>
-						<span>Lise Takviye</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/universite-takviye" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-universite.svg" alt="Üniversite Takviye"/>
-						<span>Üniversite Takviye</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/muzik" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-muzik.svg" alt="Müzik"/>
-						<span>Müzik</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/oyun-ve-hobi" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-oyunhobi.svg" alt="Oyun & Hobi"/>
-						<span>Oyun & Hobi</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/ozel-egitim" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-ozelegitim.svg" alt="Özel Eğitim"/>
-						<span>Özel Eğitim</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/sanat" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-sanat.svg" alt="Sanat"/>
-						<span>Sanat</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/sinav-hazirlik" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-sinavhazirlik.svg" alt="Sınav Hazırlık"/>
-						<span>Sınav Hazırlık</span>
-					</a>
-				</SplideSlide>
-				<SplideSlide class="text-center">
-					<a href="/ozel-ders-ilanlari-verenler/spor" class="text-blue-700 hover:text-blue-900">
-						<img src="{import.meta.env.VITE_CDN_URL}home-icon-spor.svg" alt="Spor"/>
-						<span>Spor</span>
-					</a>
-				</SplideSlide>
-			</Splide>
+			<a href="/ozel-ders-ilanlari-verenler/kisisel-gelisim" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-kisiselgelisim.svg" alt="Kişisel Gelişim"/>
+				<span>Kişisel Gelişim</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/muzik" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-muzik.svg" alt="Müzik"/>
+				<span>Müzik</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/oyun-ve-hobi" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-oyunhobi.svg" alt="Oyun & Hobi"/>
+				<span>Oyun & Hobi</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/ozel-egitim" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-ozelegitim.svg" alt="Özel Eğitim"/>
+				<span>Özel Eğitim</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/sanat" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-sanat.svg" alt="Sanat"/>
+				<span>Sanat</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/sinav-hazirlik" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-sinavhazirlik.svg" alt="Sınav Hazırlık"/>
+				<span>Sınav Hazırlık</span>
+			</a>
+			<a href="/ozel-ders-ilanlari-verenler/spor" class="text-blue-700 hover:text-blue-900">
+				<img src="{import.meta.env.VITE_CDN_URL}home-icon-spor.svg" alt="Spor"/>
+				<span>Spor</span>
+			</a>
 		</div>
 	</div>
 </section>
--- src/routes/nasil-calisir/+page.svelte
+++ src/routes/nasil-calisir/+page.svelte
@@ -4,12 +4,15 @@
 
 <div class="bg-white rounded-lg shadow-md mt-4">
 	<div class="bg-[#fbfcff] border-b border-gray-100 p-6 rounded-t-lg text-lg font-semibold">Nasıl Çalışır?</div>
+	<div class="hidden lg:block p-6">
+		<p class="text-2xl font-semibold">Öğretmen ara, öğretmen seç, randevulaş ve hemen derse başla!</p>
+		<p class="text-lg mt-2">Netders.com dört kolay adımda özel ders alma sürecinde sana birçok kolaylık sağlıyor. Netders.com'da onaylı binlerce uzman öğretmen özel ders vermek için talep oluşturmanı bekliyor.</p>
+	</div>
 	<div class="p-6">
-		<p class="hidden lg:block">Netders.com'da öğrenmek istediğin özel derse dört kolay adımda başlayabilirsin. Alanında uzman binlerce öğretmenin yer aldığı Netders.com'da tüm öğretmenler titizlikle incelenerek onaylanır. Profilini beğendiğin öğretmenden hemen ders almaya başlayabilirsin.</p>
 		<div class="grid grid-cols-2 gap-4 items-center">
 			<div>
-				<h2 class="font-semibold lg:text-[40px]">Adım 1</h2>
-				<p>Netders.com'un gelişmiş özelliklerdeki öğretmen arama bölümü ile almak istediğin özel dersi ara.</p>
+				<h2 class="font-semibold lg:text-[40px] text-blue-700">Öğretmen ara</h2>
+				<p class="lg:text-xl">Netders.com'un gelişmiş özelliklerdeki öğretmen arama bölümü ile almak istediğin özel dersi ara.</p>
 			</div>
 			<div>
 				<img src="{import.meta.env.VITE_CDN_URL}nasil-calisir-1.png" alt="Öğretmen ara" />
@@ -18,12 +21,12 @@
 				<img src="{import.meta.env.VITE_CDN_URL}nasil-calisir-2.png" alt="Öğretmen seç" />
 			</div>
 			<div>
-				<h2 class="font-semibold lg:text-[40px]">Adım 2</h2>
-				<p>Aradığın özel ders kriterlerine uygun öğretmenleri inceleyerek, beğendiğin öğretmene ders talebi oluştur.</p>
+				<h2 class="font-semibold lg:text-[40px] text-blue-700">Öğretmen seç</h2>
+				<p class="lg:text-xl">Aradığın özel ders kriterlerine uygun öğretmenleri inceleyerek, beğendiğin öğretmene ders talebi oluştur.</p>
 			</div>
 			<div>
-				<h2 class="font-semibold lg:text-[40px]">Adım 3</h2>
-				<p>Ders almayı kabul ettiğin öğretmen ile deneme dersi randevusu oluştur.</p>
+				<h2 class="font-semibold lg:text-[40px] text-blue-700">Randevulaş</h2>
+				<p class="lg:text-xl">Ders almayı kabul ettiğin öğretmen ile deneme dersi randevusu oluştur.</p>
 			</div>
 			<div>
 				<img src="{import.meta.env.VITE_CDN_URL}nasil-calisir-3.png" alt="Randevulaş" />
@@ -32,11 +35,17 @@
 				<img src="{import.meta.env.VITE_CDN_URL}nasil-calisir-4.png" alt="Ders başla" />
 			</div>
 			<div>
-				<h2 class="font-semibold lg:text-[40px]">Adım 4</h2>
-				<p>Deneme dersinde öğretmeni beğendiysen ders almaya başla veya ders vermek isteyen diğer öğretmenler ile devam et.</p>
+				<h2 class="font-semibold lg:text-[40px] text-blue-700">Derse başla</h2>
+				<p class="lg:text-xl">Öğretmenin ders verme şeklini sevdiysen onunla devam et veya talebindeki başka bir öğretmeni dene.</p>
 			</div>
+		</div>
+		<div class="mx-auto text-center">
+			<a href="/ozel-ders-ilanlari-verenler" class="bg-blue-700 hover:bg-blue-900 py-2 px-4 text-sm md:text-lg md:py-3 md:px-6 text-center rounded-full justify-center text-white block md:inline-block">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mr-1 inline-block">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+				</svg>
+				Öğretmen aramaya başla
+			</a>
 		</div>
 	</div>
 </div>
-
-
