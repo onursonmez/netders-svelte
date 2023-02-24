7b488c3d75c794c56477260cc5fc4a338336ad17
onursonmez <tavhane@gmail.com>
2023-02-24 23:57:06 +0300

    home category broken links fixed

--- src/components/home/Categories.svelte
+++ src/components/home/Categories.svelte
@@ -11,60 +11,60 @@
 		</p>
 
 		<div class="text-center grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-7 gap-6 p-6">
-			<a href="/ozel-ders/ilkogretim-takviye" class="text-blue-700 hover:text-blue-900">
+			<a href="/ilkogretim-takviye-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-ilkogretim.svg" alt="İlköğretim Takviye"/>
 				<span>İlköğretim Takviye</span>
 			</a>
-			<a href="/ozel-ders/lise-takviye" class="text-blue-700 hover:text-blue-900">
+			<a href="/lise-takviye-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-lise.svg" alt="Lise Takviye"/>
 				<span>Lise Takviye</span>
 			</a>
-			<a href="/ozel-ders/universite-takviye" class="text-blue-700 hover:text-blue-900">
+			<a href="/universite-takviye-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-universite.svg" alt="Üniversite Takviye"/>
 				<span>Üniversite Takviye</span>
 			</a>
-			<a href="/ozel-ders/yabanci-dil" class="text-blue-700 hover:text-blue-900">
+			<a href="/yabanci-dil-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-yabancidil.svg" alt="Yabancı Dil"/>
 				<span>Yabancı Dil</span>
 			</a>
-			<a href="/ozel-ders/bilgisayar" class="text-blue-700 hover:text-blue-900">
+			<a href="/bilgisayar-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-bilgisayar.svg" alt="Bilgisayar"/>
 				<span>Bilgisayar</span>
 			</a>
-			<a href="/ozel-ders/dans" class="text-blue-700 hover:text-blue-900">
+			<a href="/dans-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-dans.svg" alt="Dans"/>
 				<span>Dans</span>
 			</a>
-			<a href="/ozel-ders/direksiyon" class="text-blue-700 hover:text-blue-900">
+			<a href="/direksiyon-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-direksiyon.svg" alt="Direksiyon"/>
 				<span>Direksiyon</span>
 			</a>
 
-			<a href="/ozel-ders/kisisel-gelisim" class="text-blue-700 hover:text-blue-900">
+			<a href="/kisisel-gelisim-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-kisiselgelisim.svg" alt="Kişisel Gelişim"/>
 				<span>Kişisel Gelişim</span>
 			</a>
-			<a href="/ozel-ders/muzik" class="text-blue-700 hover:text-blue-900">
+			<a href="/muzik-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-muzik.svg" alt="Müzik"/>
 				<span>Müzik</span>
 			</a>
-			<a href="/ozel-ders/oyun-ve-hobi" class="text-blue-700 hover:text-blue-900">
+			<a href="/oyun-ve-hobi-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-oyunhobi.svg" alt="Oyun & Hobi"/>
 				<span>Oyun & Hobi</span>
 			</a>
-			<a href="/ozel-ders/ozel-egitim" class="text-blue-700 hover:text-blue-900">
+			<a href="/ozel-egitim-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-ozelegitim.svg" alt="Özel Eğitim"/>
 				<span>Özel Eğitim</span>
 			</a>
-			<a href="/ozel-ders/sanat" class="text-blue-700 hover:text-blue-900">
+			<a href="/sanat-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-sanat.svg" alt="Sanat"/>
 				<span>Sanat</span>
 			</a>
-			<a href="/ozel-ders/sinav-hazirlik" class="text-blue-700 hover:text-blue-900">
+			<a href="/sinav-hazirlik-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-sinavhazirlik.svg" alt="Sınav Hazırlık"/>
 				<span>Sınav Hazırlık</span>
 			</a>
-			<a href="/ozel-ders/spor" class="text-blue-700 hover:text-blue-900">
+			<a href="/spor-ozel-ders" class="text-blue-700 hover:text-blue-900">
 				<img src="{import.meta.env.VITE_CDN_URL}home-icon-spor.svg" alt="Spor"/>
 				<span>Spor</span>
 			</a>
