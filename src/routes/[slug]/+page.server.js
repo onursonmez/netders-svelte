48d69f2ed095e3d15c5b68a7050580e59f7c73da
onursonmez <tavhane@gmail.com>
2023-02-25 04:11:56 +0300

    user status

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -10,9 +10,15 @@ export async function load({ locals, params })
     }
 
     const user = await api.get('user/detail?username=' + params.slug, locals.auth?.token)
+
     if(user.code !== 200){
         throw error(404);
     }
+
+    if(user.result.status.id === 4){
+        throw redirect(301, '/ozel-ders');
+    }
+
     const prices = await api.get('price/' + user.result.uuid, locals.auth?.token)
     const locations = await api.get('location/' + user.result.uuid, locals.auth?.token)
     const comments = await api.get('comment/' + user.result.uuid, locals.auth?.token)
