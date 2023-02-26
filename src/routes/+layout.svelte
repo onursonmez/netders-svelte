49e4b5181faab79e3107eb4f5d2832d7098d4686
onursonmez <tavhane@gmail.com>
2023-02-26 13:41:15 +0300

    preconnect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -45,6 +45,8 @@
 		})
 	</script>
 	<link rel="canonical" href="{import.meta.env.VITE_BASE_URL}" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link rel="preconnect" href="https://cdn.netders.com" />
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 </svelte:head>
 
