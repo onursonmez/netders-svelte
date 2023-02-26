96704d058409354879a5dd03a5fc3243dc226dd2
onursonmez <tavhane@gmail.com>
2023-02-26 14:21:20 +0300

    clarity

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,13 @@
 </script>
 
 <svelte:head>
+	<link rel="canonical" href="{import.meta.env.VITE_BASE_URL}" />
+	<link rel="dns-prefetch" href="https://fonts.gstatic.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link rel="dns-prefetch" href="https://cdn.netders.com" />
+	<link rel="preconnect" href="https://cdn.netders.com" />
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+
 	<script>
 		partytown = {
 			forward: ['dataLayer.push'],
@@ -28,28 +35,14 @@
 	</script>
 
 	<script bind:this={scriptEl}></script>
-	<!-- GTM script + config -->
-	<script
-			type="text/partytown"
-			src="https://www.googletagmanager.com/gtag/js?id=G-N12QC6M7CQ"></script>
+
 	<script type="text/partytown">
-		window.dataLayer = window.dataLayer || []
-
-		function gtag() {
-			dataLayer.push(arguments)
-		}
-
-		gtag('js', new Date())
-		gtag('config', 'G-N12QC6M7CQ', {
-			page_path: window.location.pathname
-		})
+		(function(c,l,a,r,i,t,y){
+			c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+			t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+			y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+		})(window, document, "clarity", "script", "g08l2i145x");
 	</script>
-	<link rel="canonical" href="{import.meta.env.VITE_BASE_URL}" />
-	<link rel="dns-prefetch" href="https://fonts.gstatic.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" />
-	<link rel="dns-prefetch" href="https://cdn.netders.com" />
-	<link rel="preconnect" href="https://cdn.netders.com" />
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 </svelte:head>
 
 {#if $navigating}
