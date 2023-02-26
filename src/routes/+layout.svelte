3c79e7eab1436d4053a74e24fe7cc48c7fbf6b23
onursonmez <tavhane@gmail.com>
2023-02-26 16:18:12 +0300

    clarity

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -49,6 +49,16 @@
 			page_path: window.location.pathname
 		})
 	</script>
+
+	{#if import.meta.env.VITE_API_URL === 'https://api.netders.com'}
+	<script type="text/javascript">
+		(function(c,l,a,r,i,t,y){
+			c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+			t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+			y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+		})(window, document, "clarity", "script", "g08l2i145x");
+	</script>
+	{/if}
 </svelte:head>
 
 {#if $navigating}
